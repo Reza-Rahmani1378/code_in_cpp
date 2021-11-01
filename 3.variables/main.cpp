e4aa94c05712ea83333bd4cf218db377ed499eba
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-01 17:05:07 +0300

    variables

---
+++ 3.variables/main.cpp
@@ -0,0 +1,13 @@
+#include "iostream"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  string characterName = "Mohammad Reza";
+  int characterAge;
+  characterAge = 27;
+  std::cout << "there once was a man named " << characterName << std::endl;
+  std::cout << "He was " << characterAge << " years old" << std::endl;
+  std::cout << "He like the name " << characterName << std::endl;
+  std::cout << "but did not like being " << characterAge << std::endl;
+  return 0;
+}

5fdec8bc80e855eb6a43340c11c29a8678146379
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-01 21:27:32 +0300

    bug fixed

--- 7.get_user_input/main.cpp
+++ 7.get_user_input/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char *argv[]) {
   string fullName;
   int age;
   std::cout << "Enter your full name: ";
-  std::cin >> fullName;
+  getline(cin, fullName);
   std::cout << "Enter your age: ";
   std::cin >> age;
   std::cout << "You are " << fullName << " and you are " << age

c391d58078bba9ee48b53cf46dd53e5625580dde
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-01 11:48:19 +0300

    shapes

---
+++ 2.drawing_a_shape/main.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  std::cout << "    /|" << std::endl;
+  std::cout << "   / |" << std::endl;
+  std::cout << "  /  |" << std::endl;
+  std::cout << " /   |" << std::endl;
+  std::cout << "/____|" << std::endl;
+  return 0;
+}

afb5b402bbefd87fd5a6116857f7b77c0bd8832d
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-01 21:01:59 +0300

    get user input

---
+++ 7.get_user_input/main.cpp
@@ -0,0 +1,14 @@
+#include <cmath>
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
+  string fullName;
+  int age;
+  std::cout << "Enter your full name: ";
+  std::cin >> fullName;
+  std::cout << "Enter your age: ";
+  std::cin >> age;
+  std::cout << "You are " << fullName << " and you are " << age
+            << " years old.";
+  return 0;
+}

c2e7b9e7d9aec66fd9e58504733c65924104c390
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-01 19:29:44 +0300

    data type

---
+++ 4.data_type/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+  char c = 'c';
+  string s = "string";
+  int i = 1;
+  double d = 2.5;
+  bool b = true;
+  std::cout << c << std::endl;
+  std::cout << s << std::endl;
+  std::cout << i << std::endl;
+  std::cout << d << std::endl;
+  std::cout << b << std::endl;
+  return 0;
+}

65e24558ce35f594518e02bf4de2db1bc645e730
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-04 09:55:19 +0300

    for loop

---
+++ 19.for_loops/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
+  int num[] = {12, 8, 24, 2, 4, 5, 6};
+  for (int i = 0; i < 7; i++) {
+    std::cout << num[i] << std::endl;
+  }
+  return 0;
+}

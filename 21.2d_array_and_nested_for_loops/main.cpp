3f8499a33546556edad491a172bc206f75d5a892
mohammadreza booshehri <mrbooshehri@gmail.com>
2021-11-04 10:43:11 +0300

    2d array and nested for loops

---
+++ 21.2d_array_and_nested_for_loops/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char *argv[]) {
+  int array[3][2] = {{1, 2}, {3, 4}, {5, 6}};
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 2; j++) {
+      cout << array[i][j] << "\t";
+    }
+    cout << endl;
+  }
+  return 0;
+}
